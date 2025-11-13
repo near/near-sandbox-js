5aea0b5ca45fd7fcf5f46d2e4aaf8e50b0584470
r-near <163825889+r-near@users.noreply.github.com>
2025-11-12 23:23:41 -0800

    chore: remove dist files from git (#9)
    - Add dist/ to .gitignore
    - Remove all dist files from git tracking
    - Dist files will be built during CI/CD and local development

--- dist/binary/binaryExecution.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="node" />
-import { ChildProcess, StdioOptions } from "child_process";
-export declare function initConfigsWithVersion(version: string, dirPath: string): Promise<void>;
-export declare function spawnWithArgsAndVersion(version: string, args: string[], stdio?: StdioOptions): Promise<ChildProcess>;
--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { SandboxConfig, GenesisAccount, DEFAULT_ACCOUNT_ID, DEFAULT_PUBLIC_KEY, DEFAULT_PRIVATE_KEY, DEFAULT_BALANCE } from './sandbox/config';
-export * from './sandbox/Sandbox';
