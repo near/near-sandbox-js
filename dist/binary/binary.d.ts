78e87711effebb00155af50c0a13746e28e7d63f
r-near <163825889+r-near@users.noreply.github.com>
2025-11-30 02:25:35 -0800

    chore: remove dist files from git (#21)
    - Add dist/ to .gitignore
    - Remove all dist files from git tracking
    - Dist files will be built during CI/CD and local development

--- dist/binary/binary.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function downloadBin(version: string): Promise<string>;
-export declare function binPath(version: string): Promise<string>;
-export declare function checkForVersion(version: string): Promise<string | undefined>;
-export declare function installable(binPath: string): Promise<(() => Promise<void>) | null>;
-export declare function ensureBinWithVersion(version: string): Promise<string>;
--- dist/binary/binaryUtils.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function AWSUrl(version?: string): string;
-export declare function fileExists(filePath: string): Promise<boolean>;
